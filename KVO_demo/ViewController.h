d124fd4920801ad1446c085fce02d502c688e698
litianqi <871651575@qq.com>
2018-07-18 14:45:19 +0800

    Initial Commit

---
+++ KVO_demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KVO_demo
+//
+//  Created by litianqi on 2018/7/18.
+//  Copyright © 2018年 tqUDown. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ KVO_demo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  KVO_demo
+//
+//  Created by litianqi on 2018/7/18.
+//  Copyright © 2018年 tqUDown. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
